00cf3d6ddc8d1c2820990050a57b433d3f1491e9
Christopher Sidebottom <chris.sidebottom@arm.com>
2026-03-17 15:53:25 +0000

    fix: Do not reload plugins in plugin init  (#12)
    * fix: Do not reload plugins in plugin init
    
    Plugins are autoloaded as part of importing the relevant bit of `mlia-core`
    
    * fix: Ensure we test against the requested wheel

--- src/mlia/backend/__init__.py
+++ src/mlia/backend/__init__.py
@@ -6,8 +6,3 @@ import pkgutil
 
 # Allow backend subpackages to be provided by multiple distributions.
 __path__ = pkgutil.extend_path(__path__, __name__)
-
-from mlia.backend.registry import registry
-from mlia.plugins.plugins import load_backend_plugins
-
-load_backend_plugins(registry)
